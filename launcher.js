cfbcc32eacae0dcc89ad4924cc647ffa5829f55d
unknown <victorst79@gmail.com>
2018-10-19 12:45:40 +0200

    launcher.js created, bot run with Node server

---
+++ launcher.js
@@ -0,0 +1,19 @@
+try{
+    var Discord = require('discord.js');
+    var client = new Discord.Client();
+}catch(e){
+    console.log(e.stack);
+	console.log(process.version);
+	console.log("Please run npm install and ensure it passes with no errors!");
+	process.exit();
+}
+
+console.log("Starting NaM-Bot\nNode version: " + process.version + "\nDiscord.js version: " + Discord.version);
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag)
+});
+
+bot_secret_token = "TOKEN";
+
+client.login(bot_secret_token);
