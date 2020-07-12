50ef2d031cb7d141a83bd594fd53586385aa9bf3
Victor Santaella <victorst79@gmail.com>
2020-07-12 14:47:18 +0200

    another token leak

--- src/Settings/settings.ts
+++ src/Settings/settings.ts
@@ -1,4 +1,4 @@
-export let token: string = "NzExMTU1NzI4OTExNDMzNzY5.Xu4e3g.wx4beosgUNogOIM-0A-TIJCwjXI";
+export let token: string = "REPLACE TOKEN";
 export let prefix: string = "!";
 export let owners: string[] = [];
 export let colorFeedback: string = "BLUE";
