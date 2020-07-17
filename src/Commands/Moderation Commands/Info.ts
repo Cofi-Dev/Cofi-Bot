454ef692548d1df1c15ef77316c0486b43e5c72f
Victor Santaella <victorst79@gmail.com>
2020-07-17 18:51:17 +0200

    testing some rbac checks in info command

--- src/Commands/Moderation Commands/Info.ts
+++ src/Commands/Moderation Commands/Info.ts
@@ -1,5 +1,5 @@
 import { Command } from "discord-akairo";
-import { Message, MessageEmbed } from "discord.js";
+import { Message, MessageEmbed, User } from "discord.js";
 
 export default class Info extends Command {
   public constructor() {
@@ -7,11 +7,31 @@ export default class Info extends Command {
       aliases: ["Info"],
       category: "Moderation Commands",
       description: {
-        content: "Info about user",
-        usage: "info",
+        content: "Info about member",
+        usage: "info @member",
         examples: ["info"],
       },
       ratelimit: 3,
+      args: [
+        {
+          id: "member",
+          type: "member",
+          match: "rest",
+        },
+      ],
     });
+  }
+
+  public async exec(message: Message, { member }: { member }): Promise<Message> {
+    if (member) {
+      console.log(member.GuildMember);
+      return message.util.send(
+        new MessageEmbed().setTitle(`Bot Uptime`).setColor("GREEN").setDescription(`Info about member...`)
+      );
+    } else {
+      return message.util.send(
+        new MessageEmbed().setTitle(`Bot Uptime`).setColor("GREEN").setDescription(`Invalid user`)
+      );
+    }
   }
 }
