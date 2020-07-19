568b5698e965151891368185a8af961ea16d4b19
Victor Santaella <victorst79@gmail.com>
2020-07-19 12:25:44 +0200

    another token leak !!

--- src/Settings/settings.ts
+++ src/Settings/settings.ts
@@ -1,4 +1,4 @@
-export let token: string = "NzExMTU1NzI4OTExNDMzNzY5.XwsGYg._iRgE_1qjULADHy9D7soR55_ioE";
+export let token: string = "REPLACE TOKEN";
 export let prefix: string = "!";
 export let owners: string[] = [];
 export let color: string = "BLUE";
