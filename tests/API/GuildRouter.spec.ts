13f95e7fc22f88202191701027276e2c36160fde
Victor Santaella <victorst79@gmail.com>
2020-12-09 20:25:02 +0100

    added settings for test

--- tests/API/GuildRouter.spec.ts
+++ tests/API/GuildRouter.spec.ts
@@ -2,10 +2,11 @@ import 'mocha';
 var chai = require('chai'), chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const expect = chai.expect;
+import { server } from '../test_config'
 
 describe('Guild ID', () => {
   it('should return response on call', (done) => {
-    return chai.request("http://localhost:8080").get('/guild/699517667177922571')
+    return chai.request(server).get('/guild/699517667177922571')
       .then(res => {
         expect(res).to.have.status(200);
       })
---
+++ tests/test_config.ts
@@ -0,0 +1,1 @@
+export const server: String = "http://localhost:8888";
