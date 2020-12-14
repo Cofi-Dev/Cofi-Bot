c3e4092b9652418b0d6be8a89331e4325b7d5239
Victor Santaella <victorst79@gmail.com>
2020-12-14 21:28:46 +0100

    format duration and testint docker deploy

--- src/Utils/Interfaces/Duration.ts
+++ src/Utils/Interfaces/Duration.ts
@@ -1,10 +1,10 @@
 export interface Duration extends moment.Duration {
-    format: (template?: string, precision?: number, settings?: DurationSettings) => string;
-  }
-  
+  format: (template?: string, precision?: number, settings?: DurationSettings) => string;
+}
+
 export interface DurationSettings {
-forceLength: boolean;
-precision: number;
-template: string;
-trim: boolean | "left" | "right";
+  forceLength: boolean;
+  precision: number;
+  template: string;
+  trim: boolean | "left" | "right";
 }
