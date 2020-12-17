8bb3f95cd5a0790fa8e9e68746c0a340acd722a8
Victor Santaella <victorst79@gmail.com>
2020-12-17 21:28:08 +0100

    fixed all api test and add 404 error

--- tests/API/GuildRouter.spec.ts
+++ tests/API/GuildRouter.spec.ts
@@ -5,12 +5,22 @@ const expect = chai.expect;
 import { server } from '../test_config'
 
 describe('Guild ID', () => {
-  it('should return response on call', done => {
+  it('Return a JSON, with the basic data of a guild', done => {
     chai
       .request(server)
       .get('/guild/699517667177922571')
       .end((err, res) => {
         expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('Return a 404 with fake Guild-id', done => {
+    chai
+      .request(server)
+      .get('/guild/699517667177922570')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
         done();
       });
   })
