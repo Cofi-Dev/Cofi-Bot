c97cec727fcf2d6fad36d9698cef6909751e5bc1
Victor Santaella <victorst79@gmail.com>
2021-01-20 20:01:14 +0100

    new command added

---
+++ src/Commands/Public Commands/Posture.ts
@@ -0,0 +1,24 @@
+import { Command } from "discord-akairo";
+import { Message, MessageEmbed } from "discord.js";
+import { color } from "../../Settings";
+
+export default class Posture extends Command {
+  public constructor() {
+    super("posture", {
+      aliases: ["posture", "postura", "fixposture"],
+      category: "Public Commands",
+      description: {
+        content: "Friendly reminder to improve chair posture.",
+        usage: "posture",
+        examples: ["posture"],
+      },
+      ratelimit: 3,
+    });
+  }
+
+  public async exec(message: Message): Promise<Message> {
+    return await message.util.send(
+      new MessageEmbed().setTitle(`GUYS REMEMBER`).setDescription("@everyone fix ur posture").setColor(color)
+    );
+  }
+}
