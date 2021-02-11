4aaea81dbdb7498e4d3859babf39538581296c85
victorst79 <victorst79@gmail.com>
2021-02-11 10:11:59 +0100

    add version command

---
+++ src/Commands/Private Commands/Version.ts
@@ -0,0 +1,24 @@
+import { Command } from "discord-akairo"
+import { Message } from "discord.js"
+
+export default class Ping extends Command {
+  public constructor() {
+    super("version", {
+      aliases: ["Version"],
+      category: "Private Commands",
+      description: {
+        content: "Return bot version",
+        usage: "version",
+        examples: ["version"],
+      },
+      userPermissions: ["ADMINISTRATOR", "MANAGE_GUILD"],
+      ratelimit: 3,
+    })
+  }
+
+  public async exec(message: Message): Promise<Message> {
+    return message.util.send(
+      `Version: ${process.env.npm_package_version}\nEnviroment: ${process.env.npm_lifecycle_event}`
+    )
+  }
+}
